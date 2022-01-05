67c77aa1ecd43fa00fe136718c852ae01efbc8b7
RunanWang <1070623719@qq.com>
2022-01-05 20:35:26 +0800

    Feat: Add urltop10

--- lab0/urltop10.go
+++ lab0/urltop10.go
@@ -1,9 +1,93 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // URLTop10 .
 func URLTop10(nWorkers int) RoundsArgs {
 	// YOUR CODE HERE :)
 	// And don't forget to document your idea.
-	panic("YOUR CODE HERE")
-	return nil
+	var args RoundsArgs
+	// round 1: do url count
+	args = append(args, RoundArgs{
+		MapFunc:    URLCountMap,
+		ReduceFunc: URLCountReduce,
+		NReduce:    nWorkers,
+	})
+	// round 2: sort and get the 10 most frequent URLs
+	args = append(args, RoundArgs{
+		MapFunc:    URLTop10Map,
+		ReduceFunc: URLTop10Reduce,
+		NReduce:    1,
+	})
+	return args
+}
+
+func URLCountMap(filename string, contents string) []KeyValue {
+	lines := strings.Split(contents, "\n")
+	cnts := make(map[string]int)
+	for _, l := range lines {
+		l = strings.TrimSpace(l)
+		if len(l) == 0 {
+			continue
+		}
+		if v, ok := cnts[l]; ok {
+			cnts[l] = v + 1
+		} else {
+			cnts[l] = 1
+		}
+	}
+	kvs := make([]KeyValue, 0, len(cnts))
+	for k, v := range cnts {
+		kvs = append(kvs, KeyValue{Key: k, Value: strconv.Itoa(v)})
+	}
+	return kvs
+}
+
+func URLCountReduce(key string, values []string) string {
+	ret := 0
+	for i := 0; i < len(values); i++ {
+		n, err := strconv.Atoi(values[i])
+		if err != nil {
+			panic(err)
+		}
+		ret = ret + n
+	}
+	return fmt.Sprintf("%s %s\n", key, strconv.Itoa(ret))
+}
+
+func URLTop10Map(filename string, contents string) []KeyValue {
+	lines := strings.Split(contents, "\n")
+	kvs := make([]KeyValue, 0, len(lines))
+	for _, l := range lines {
+		kvs = append(kvs, KeyValue{"", l})
+	}
+	return kvs
+}
+
+func URLTop10Reduce(key string, values []string) string {
+	cnts := make(map[string]int, len(values))
+	for _, v := range values {
+		v := strings.TrimSpace(v)
+		if len(v) == 0 {
+			continue
+		}
+		tmp := strings.Split(v, " ")
+		n, err := strconv.Atoi(tmp[1])
+		if err != nil {
+			panic(err)
+		}
+		cnts[tmp[0]] = n
+	}
+
+	us, cs := TopN(cnts, 10)
+	buf := new(bytes.Buffer)
+	for i := range us {
+		fmt.Fprintf(buf, "%s: %d\n", us[i], cs[i])
+	}
+	return buf.String()
 }
